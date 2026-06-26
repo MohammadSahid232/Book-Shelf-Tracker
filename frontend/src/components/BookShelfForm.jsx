722571843a178d6949e9e554cc42d024642315f1
Mohammad Sahid Rain <samshedkhan741@gmail.com>
2026-06-26 11:47:54 +0545

    upadated main

--- frontend/src/components/BookShelfForm.jsx
+++ frontend/src/components/BookShelfForm.jsx
@@ -5,13 +5,13 @@ export default function Form() {
       <div>
         <label htmlFor="full-name-small" className="mb-2 text-slate-900 dark:text-slate-50 font-bold text-sm inline-block">Book Name
         </label>
-        <input type="text" id="full-name-small" placeholder="Habit Name"
+        <input type="text" id="full-name-small" placeholder="Book Name"
           className="px-2.5 py-2 text-sm text-slate-900 dark:text-slate-50 rounded-md bg-white dark:bg-neutral-800 w-full outline-1 -outline-offset-1 outline-slate-300 dark:outline-neutral-700 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600" />
       </div>
       <div>
         <label htmlFor="full-name-small" className="mb-2 text-slate-900 dark:text-slate-50 font-bold text-sm inline-block">Book Description
         </label>
-        <input type="text" id="full-name-small" placeholder="Habit Description"
+        <input type="text" id="full-name-small" placeholder="Book Description"
           className="px-2.5 py-2 text-sm text-slate-900 dark:text-slate-50 rounded-md bg-white dark:bg-neutral-800 w-full outline-1 -outline-offset-1 outline-slate-300 dark:outline-neutral-700 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600" />
       </div>
       <button type="submit" className="btn btn-active btn-primary bg-blue-200">Submit</button>
