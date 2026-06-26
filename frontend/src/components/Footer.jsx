247696edb6da8ede8a565b2dbe3ddb8d5e4cdc2e
Mohammad Sahid Rain <samshedkhan741@gmail.com>
2026-06-26 11:51:32 +0545

    upadated week1

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="footer sm:footer-horizontal bg-blue-300 text-base-content p-4 text-center h-[10vh] m-0 ">
+        <footer className="footer footer-center bg-blue-300 text-base-content p-4 h-[10vh] m-0">
             <aside>
                 <p>Copyright © {new Date().getFullYear()} - All right reserved by Book Shelf Ltd</p>
             </aside>
