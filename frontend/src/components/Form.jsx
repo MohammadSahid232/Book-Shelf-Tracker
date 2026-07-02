bbc7028bb89587f541b614dfc710d6de30b6fd4b
Mohammad Sahid Rain <samshedkhan741@gmail.com>
2026-07-02 11:02:37 +0545

    refactor: Migrate BookShelfForm styling to Form.jsx and remove BookShelfForm

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Form({ onSubmit, fields, submitText }) {
   return (
-    <form onSubmit={onSubmit} className="flex flex-col justify-center w-full space-y-6">
+    <form onSubmit={onSubmit} className="flex flex-col justify-center max-w-lg mx-auto p-10 space-y-6">
       {fields.map((field, index) => (
         <div key={index}>
-          <label htmlFor={field.name} className="mb-2 text-slate-900 font-bold text-sm inline-block">
+          <label htmlFor={field.name} className="mb-2 text-slate-900 dark:text-slate-50 font-bold text-sm inline-block">
             {field.label}
           </label>
           <input
@@ -14,11 +14,11 @@ export default function Form({ onSubmit, fields, submitText }) {
             placeholder={field.placeholder}
             value={field.value}
             onChange={field.onChange}
-            className="px-2.5 py-2 text-sm text-slate-900 rounded-md bg-white w-full outline-1 -outline-offset-1 outline-slate-300 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 border border-gray-300"
+            className="px-2.5 py-2 text-sm text-slate-900 dark:text-slate-50 rounded-md bg-white dark:bg-neutral-800 w-full outline-1 -outline-offset-1 outline-slate-300 dark:outline-neutral-700 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
           />
         </div>
       ))}
-      <button type="submit" className="btn btn-primary w-full">
+      <button type="submit" className="btn btn-active btn-primary bg-blue-200">
         {submitText}
       </button>
     </form>
